f5a7c938f47eb25fbd1286a7b89d9d71bebc9902
codingarchitect <sendhil.r@gmail.com>
2018-08-03 11:59:04 +0530

    Hack to stagger loading

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+function dispatchGitHubActions(dispatch) {
+  window.setTimeout(() => {
+    dispatch({
+      type: 'LOAD_GITHUB_USER',
+      payload: 'codingarchitect',
+    });
+  }, 0);
+  window.setTimeout(() => {
+    dispatch({
+      type: 'LOAD_GITHUB_USER',
+      payload: 'sonyarouje',
+    });
+  }, 500);
+  window.setTimeout(() => {
+    dispatch({
+      type: 'LOAD_GITHUB_USER',
+      payload: 'staltz',
+    });
+  }, 1000);
+}
+
 const App = ({ dispatch }) => (
   <div>
     <h1>Hello from react!</h1>
     <button onClick={() => dispatch({ type: 'PING' })}>Dispatch Ping</button>
-    <button onClick={() => {
-      dispatch({ type: 'LOAD_GITHUB_USER', payload: 'codingarchitect' });
-      dispatch({ type: 'LOAD_GITHUB_USER', payload: 'sonyarouje' });
-      dispatch({ type: 'LOAD_GITHUB_USER', payload: 'staltz' });
-    }}
-    >Dispatch Load Github User: codingarchitect</button>
+    <button onClick={() => dispatchGitHubActions(dispatch)}>
+      Dispatch Load Github User: codingarchitect
+    </button>
     <div>Check the console to see if ping was mapped to pong by cycle on Dispatch Ping</div>
   </div>
 );
